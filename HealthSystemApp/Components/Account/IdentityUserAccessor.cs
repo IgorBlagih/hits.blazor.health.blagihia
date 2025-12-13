2c0efbcb9f400750fbb3e952a297199beed96953
IgorBlagih <igorblagih5@gmail.com>
2025-12-13 18:24:19 +0500

    Add project files.

---
+++ HealthSystemApp/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using HealthSystemApp.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace HealthSystemApp.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
